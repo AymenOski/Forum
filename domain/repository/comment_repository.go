688ad93ae5af4e64e4c56d77967740dd3b68b486
Nakazaki4 <nakazakinakazaki22@gmail.com>
2025-05-22 10:22:18 +0100

    Created the comment repository interface

--- domain/repository/comment_repository.go
+++ domain/repository/comment_repository.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"forum/domain/entity"
+
+	"github.com/google/uuid"
+)
+
+type CommentRepository interface {
+	Create(comment *entity.Comment) error
+	GetByID(commentID int) (*entity.Comment, error)
+	
+	GetByPostID(postID int)([]*entity.Comment, error)
+}
