729b759771027bd4d682cb60680e547e99dc4ab5
Nakazaki4 <nakazakinakazaki22@gmail.com>
2025-05-22 10:07:09 +0100

    Created user repository interface

--- domain/repository/user_repository.go
+++ domain/repository/user_repository.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"forum/domain/entity"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserRepository interface {
+	Create(user *entity.User) error
+	GetByID(userID *uuid.UUID) (*entity.User, error)
+	GetByUsername(username string) (*entity.User, error)
+	GetByEmail(email string) (*entity.User, error)
+
+	CreateSession(userID *uuid.UUID, sessionToken string, expiry time.Time) error
+	GetBySessionToken(sessionToken string) (*entity.User, error)
+}
--- infrastructure/schema/forum.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Comments (
-    comment_id TEXT PRIMARY KEY,
-    post_id TEXT NOT NULL,
-    user_id TEXT NOT NULL,
-    content TEXT NOT NULL,
-)
