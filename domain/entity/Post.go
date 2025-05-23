abb64d53983ad6f8a32a43ef222c7054bb42f52f
Mjait <mohammed.jait99@gmail.com>
2025-05-23 11:58:45 +0100

    adabted to abdnour work

--- domain/entity/Post.go
+++ domain/entity/Post.go
@@ -3,6 +3,7 @@ package entity
 type Post struct {
 	UserID        string `json:"user_id"`
 	PostID        int64  `json:"post_id"`
+	Authorname    string `json:username`
 	Content       string `json:"content"`
 	LikesCount    int    `json:"likes_count"`
 	DislikesCount int    `json:"dislikes_count"`
