52f711a6d73050ba731f299362cd691b35ab585b
AymenOski <aymenazizi610@gmail.com>
2025-05-24 12:55:33 +0100

    fix(layour) error :adding a place holder for an if condition

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,11 +9,10 @@ import (
 )
 
 func main() {
-		fmt.Println("Server started on http://localhost:8080")
+	db := database.SetingUpDB()
+	defer db.Close()
+	fmt.Println("Server started on http://localhost:8080")
 	if err := server.Froum_server().ListenAndServe(); err != nil {
 		log.Fatalf("500 - Internal Server Error: %v", err)
 	}
-	
-	db := database.SetingUpDB()
-	defer db.Close()
 }
