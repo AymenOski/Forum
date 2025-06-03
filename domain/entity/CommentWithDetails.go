a0dcefd7a84d7ec4390709d096d031e0c832d0f8
ussefbouchraa <ussefbouchraa@gmail.com>
2025-06-03 15:05:09 +0100

    set repo

--- domain/entity/CommentWithDetails.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type CommentWithDetails struct {
-	Comment
-	Author       User `json:"author"`
-	LikeCount    int  `json:"like_count"`
-	DislikeCount int  `json:"dislike_count"`
-}
--- domain/entity/PostCategory.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-import "github.com/google/uuid"
-
-type PostCategory struct {
-	PostID     uuid.UUID `json:"post_id" db:"post_id"`
-	CategoryID uuid.UUID `json:"category_id" db:"category_id"`
-}
